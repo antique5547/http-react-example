26036b4f39b18538565b4297c6e7fb33d229e627
atique2992 <mohammad.atiqurrahaman@walkingtree.in>
2019-02-24 17:35:39 +0530

    First Commit

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import "./FullPost.css";
+
+class FullPost extends Component {
+  state = {
+    post: null
+  };
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+    if (this.props.id) {
+      if (
+        !this.state.post ||
+        (this.state.post && this.state.post.id !== this.props.id)
+      ) {
+        axios
+          .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+          .then(response => {
+            console.log(response);
+            this.setState({ post: response.data });
+          });
+      }
+    }
+  }
+
+  deletePostHandler = id => {
+    axios
+      .delete("https://jsonplaceholder.typicode.com/posts/" + id)
+      .then(response => {
+        console.log("deleted succesfull : ", response);
+      });
+  };
+
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = <p style={{ textAlign: "center" }}>Loading...!</p>;
+    }
+    if (this.state.post) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.post.title}</h1>
+          <p>{this.state.post.body}</p>
+          <div className="Edit">
+            <button
+              className="Delete"
+              onClick={() => this.deletePostHandler(this.props.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      );
+    }
+
+    return post;
+  }
+}
+
+export default FullPost;
---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+
+class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  };
+
+  selectedPostHandler = id => {
+    this.setState({ selectedPostId: id });
+    console.log(this.state.selectedPostId);
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/postss")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatePosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Atique"
+          };
+        });
+        this.setState({ posts: updatePosts });
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ error: true });
+      });
+  }
+
+  render() {
+    let postHeader = (
+      <p style={{ textAlign: "center", color: "red" }}>
+        Something went wrong!!!
+      </p>
+    );
+    if (!this.state.error) {
+      postHeader = this.state.posts.map(post => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.selectedPostHandler(post.id)}
+          />
+        );
+      });
+    }
+    return (
+      <div>
+        <section className="Posts">{postHeader}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
